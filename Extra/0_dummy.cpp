e551759bfd2b7750fc746bec516fa72c4314bd4e
Krissny <kk124572@outlook.com>
2023-05-25 07:20:34 +0530

    init

--- Extra/0_dummy.cpp
+++ Extra/0_dummy.cpp
@@ -1,63 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-//implement a stack using queue
 
-struct node{
-    int data;
-    struct node* next ;
-};
-
-struct node* head = nullptr;
-void insertAtBegin(int data){
-    struct node* newNode = (struct node*)malloc(sizeof(struct node));
-    newNode->data = data;
-    newNode->next = head;
-    head = newNode;
-}
-void display(){
-    struct node* current = head;
-    cout<<"[";
-    while(current!=nullptr){
-        cout<<current->data<<" ";
-        current = current->next;
-    }
-    cout<<"]";
-}
-int getsize(){
-    int size =0;
-    struct node* current = head;
-    while(current!=nullptr){
-        current= current->next;
-        size++;
-    }
-    return size;
-}
-void insertAtgiven(int pos, int data){
-    int size = getsize();
-    struct node* newNode = (struct node*)malloc(sizeof(struct node));
-    newNode->data = data;
-    if(pos<0 and pos > size){
-        cout<<"Invalid position\n";
-    }
-    else if(pos==0){
-        newNode->next = head;
-        head = newNode;
-    }
-    else{
-        struct node* current = head;
-        while(--pos){
-            current=current->next;
+int editDistance(string s, string t) {
+        // Code here
+        int m = s.length();
+        int n = t.length();
+        int dp[m+1][n+1];
+        for(int i=0; i<=m; i++){
+            for(int j =0; j<n;j++){
+                dp[i][j]=0;
+            }
         }
-        newNode->next = current->next;
-        current->next = newNode;
+        for(int i=1; i<=m; i++){
+            for(int j =1; j<=n; j++){
+                if(s[i]==t[j]){
+                    dp[i][j]= 1 +dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        // int ans =0;
+        // if(m>=n){
+        //      ans = m - dp[m][n];
+        // }
+        // else{
+        //    ans = n - dp[m][n]; 
+        // }
+        return dp[m][n];
     }
-}
 
-int main() {
-   insertAtBegin(1);
-   insertAtBegin(2);
-   insertAtBegin(3);
-   display();
-   insertAtgiven(1,6);
-   display();
+int main(){
+    string s ="ecfbefdcfca";
+    string t ="badfcbebbf";
+    int ans = editDistance(s,t);
+    cout<<ans<<endl;
 }
