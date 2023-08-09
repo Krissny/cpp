32f5b72b0fa40ef45dc1341c301ce6a4523f6eea
Krissny <110375406+Krissny@users.noreply.github.com>
2023-08-09 10:32:35 +0530

    init

--- Extra/0_dummy.cpp
+++ Extra/0_dummy.cpp
@@ -1,30 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printBinary(int num){
-    for(int i =31; i >=0; --i){
-        cout<<((num>>i) &1); 
-    }
-    cout<<endl;
-}
-int power(int n){
-    int i =0;
-    while(n >= 1<<i){
-        i++;
-    }
-    return i-1;
-}
-int solve(int n, int p){
-    if(p<=0){
-        return 0;
-    }
-    int a = power(p);
-    int ans =(n<<a) + solve(n ,p-(1<<a));
-    return ans;
-}
+
 int main()
 {
-    int n;
-    cin>>n;
-    cout<<solve(n,n)<<endl;
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        int odd_ct =0;
+        for(int i=0; i<n; i++){
+            cin>>arr[i];
+            if(arr[i]%2!=0){
+                odd_ct++;
+            }
+        }
+        if(odd_ct %2==0){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
 }
